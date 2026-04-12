15ec4a57f29203f197599f38c0353ddfe45528df
tiffany-builds <tiffany.macrae@gmail.com>
2026-04-12 22:44:07 +0200

    Add Capacitor for iOS deployment

---
+++ capacitor.config.ts
@@ -0,0 +1,28 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.carry.app',
+  appName: 'Carry',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  ios: {
+    contentInset: 'always',
+    backgroundColor: '#E8DDD0',
+    scrollEnabled: false,
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1500,
+      backgroundColor: '#E8DDD0',
+      showSpinner: false,
+    },
+    StatusBar: {
+      style: 'Dark',
+      backgroundColor: '#E8DDD0',
+    }
+  }
+};
+
+export default config;
